ca2087e887940d6de7206465649e58398fa66a1c
Rahil2507 <rahmad.2507@gmail.com>
2022-02-15 20:20:10 +0530

    first

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Footer = () => (
+  <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200 ">
+    <a className='text-blue-700' href="https://www.linkedin.com/in/rahil-ahmad-11330780/">
+      © 2022 Gideon, Inc. - Rahil Ahmad
+    </a>
+  </div>
+  // <a className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-20" href="https://www.linkedin.com/in/rahil-ahmad-11330780/"> Rahil Ahmad</a>
+);
